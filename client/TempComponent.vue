69cc72878f7f442f5fa1449040067284600cbe46
Abdullah Omar <fishilike.fish@gmail.com>
2020-10-17 03:30:26 +0600

    mongodb items collection integration

--- client/TempComponent.vue
+++ client/TempComponent.vue
@@ -1,15 +1,26 @@
 <template>
 	<div>
-		{{ p }}
+		<ul>
+			<li v-for="(item, index) in items" :key="index">
+				{{ item.name }}
+			</li>
+		</ul>
 	</div>
 </template>
 
 <script>
+import { Items } from '../imports/api/items';
+
 export default {
 	data() {
 		return {
 			p: 'pasdasdsad',
 		};
+	},
+	meteor: {
+		items() {
+			return Items.find({}).fetch();
+		},
 	},
 };
 </script>
