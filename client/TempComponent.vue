950aabe19645791d7b6b4a3db11fd643453f1097
Abdullah Omar <fishilike.fish@gmail.com>
2020-10-17 04:05:30 +0600

    remove insecure package and integrate insert method

--- client/TempComponent.vue
+++ client/TempComponent.vue
@@ -2,20 +2,42 @@
 	<div>
 		<ul>
 			<li v-for="(item, index) in items" :key="index">
-				{{ item.name }}
+				{{ item.name }} for ${{ item.price }}
 			</li>
 		</ul>
+
+		<form @submit.prevent="saveNewItem">
+			<label for="item-name">Name</label>
+			<input type="text" id="item-name" v-model="item.name" />
+			<br />
+			<label for="item-price">Name</label>
+			<input type="text" id="item-name" v-model="item.price" />
+			<button>Submit</button>
+		</form>
 	</div>
 </template>
 
 <script>
+import { Meteor } from 'meteor/meteor';
+
 import { Items } from '../imports/api/items';
 
 export default {
 	data() {
 		return {
-			p: 'pasdasdsad',
+			item: { name: '', price: 0 },
 		};
+	},
+	methods: {
+		saveNewItem() {
+			Meteor.call('createItem', this.item, (e, res) => {
+				if (e) {
+					console.log(e);
+				} else {
+					console.log(res);
+				}
+			});
+		},
 	},
 	meteor: {
 		items() {
--- imports/api/items.js
+++ imports/api/items.js
@@ -1,3 +1,10 @@
 import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
 
 export const Items = new Mongo.Collection('items');
+
+Meteor.methods({
+	createItem(item) {
+		return Items.insert(item);
+	},
+});
