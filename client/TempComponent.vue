e139f881e61fac510064aa78034a960158c8bc14
Abdullah Omar <fishilike.fish@gmail.com>
2020-10-17 02:25:06 +0600

    Vue integration

---
+++ client/TempComponent.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		{{ p }}
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			p: 'pasdasdsad',
+		};
+	},
+};
+</script>
+
+<style lang="css" scoped></style>
---
+++ imports/ui/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<p>{{ greeting }}</p>
+		<temp-component></temp-component>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			greeting: 'Something',
+		};
+	},
+};
+</script>
+
+<style lang="css" scoped></style>
